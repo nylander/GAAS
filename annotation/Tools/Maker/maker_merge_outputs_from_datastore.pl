a29aa31fa1dbdef7e9b7a43b5d684c07b18bccfd
Jacques Dainat <jacques.dainat@bils.se>
2016-04-20 15:07:28 +0200

    fix folder name seach in maker_merge script

--- annotation/Tools/Maker/maker_merge_outputs_from_datastore.pl
+++ annotation/Tools/Maker/maker_merge_outputs_from_datastore.pl
@@ -112,7 +112,7 @@ if ($nbDir > 1 ){print "Results will be merged together !\n";}
 
 foreach my $makerDir (@inDir){
 	my $prefix = $makerDir;
-	$prefix = s/\.maker\.output//;
+	$prefix =~ s/\.maker\.output//;
 	my $maker_dir_path = $dir . "/" . $makerDir."/";
 	my $datastore = $maker_dir_path.$prefix."datastore" ;
 
