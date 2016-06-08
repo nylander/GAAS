c54d261904b8cb5398f9a2b8516a8d73b4c0a5fe
Jacques Dainat <jacques.dainat@bils.se>
2016-06-08 10:59:49 +0200

    new script to extract attribute in gff3

---
+++ annotation/Tools/Util/gff/gff3_extractAttributes.pl
@@ -0,0 +1,255 @@
+#!/usr/bin/perl
+
+
+use Carp;
+use Clone 'clone';
+use strict;
+use Getopt::Long;
+use Pod::Usage;
+use Data::Dumper;
+use List::MoreUtils qw(uniq);
+use Bio::Tools::GFF;
+use BILS::Handler::GXFhandler qw(:Ok);
+use BILS::Handler::GFF3handler qw(:Ok);
+
+my $header = qq{
+########################################################
+# BILS 2015 - Sweden                                   #  
+# jacques.dainat\@bils.se                               #
+# Please cite BILS (www.bils.se) when using this tool. #
+########################################################
+};
+
+my $gff = undef;
+my $help= 0;
+my $primaryTag=undef;
+my $attributes=undef;
+my $outfile=undef;
+
+if ( !GetOptions(
+    "help|h" => \$help,
+    "gff|f=s" => \$gff,
+    "p|t|l=s" => \$primaryTag,
+    "attributes|a|att=s" => \$attributes,
+    "output|outfile|out|o=s" => \$outfile))
+
+{
+    pod2usage( { -message => 'Failed to parse command line',
+                 -verbose => 1,
+                 -exitval => 1 } );
+}
+
+# Print Help and exit
+if ($help) {
+    pod2usage( { -verbose => 2,
+                 -exitval => 2,
+                 -message => "$header\n" } );
+}
+ 
+if ( ! (defined($gff)) ){
+    pod2usage( {
+           -message => "$header\nAt least 1 parameter is mandatory:\nInput reference gff file (--gff) \n\n",
+           -verbose => 0,
+           -exitval => 2 } );
+}
+
+my $out = IO::File->new();
+if ($outfile) {
+  $outfile=~ s/.gff//g;
+  open($out, '>', $outfile.".txt") or die "Could not open file '$outfile' $!";
+}
+else{
+  $out->fdopen( fileno(STDOUT), 'w' );
+}
+
+# Manage $primaryTag
+my @ptagList;
+if(! $primaryTag or $primaryTag eq "all"){
+  print "We will work on attributes from all features\n";
+  push(@ptagList, "all");
+}elsif($primaryTag =~/^level[123]$/){
+  print "We will work on attributes from all the $primaryTag features\n";
+  push(@ptagList, $primaryTag);
+}else{
+   @ptagList= split(/,/, $primaryTag);
+   foreach my $tag (@ptagList){
+      if($tag =~/^level[123]$/){
+        print "We will work on attributes from all the $tag features\n";
+      }
+      else{
+       print "We will work on attributes from $tag feature.\n";
+      }
+   }
+}
+
+# Manage attributes if given
+### If attributes given, parse them:
+my %attListOk;
+my @attListPair;
+if ($attributes){
+  @attListPair= split(/,/, $attributes);
+  my $nbAtt=$#attListPair+1;
+  
+  foreach my $attributeTuple (@attListPair){ 
+    my @attList= split(/\//, $attributeTuple);
+    if($#attList == 0){ # Attribute alone
+      #check for ID attribute
+      if(lc($attList[0]) eq "id"){print "It's forbidden to remove the ID attribute in a gff3 file !\n";exit;}
+      #check for Parent attribute
+      if(lc($attList[0]) eq "parent"){
+        foreach my $tag (@ptagList){
+          if($tag ne "gene" and $tag ne "level1"){
+            print "It's forbidden to remove the $attList[0] attribute to a $tag feature in a gff3 file !\n";
+            exit;
+          }
+        }
+      }
+      $attListOk{$attList[0]}++;
+      print "$attList[0] attribute will be processed.\n";
+    }
+    else{ # Attribute we have to replace by a new name
+      print "I don't understand this attribute $attList[1] !. Do not provide any \\ character.\n";
+    }
+  }
+  print "\n";
+}
+
+
+                #####################
+                #     MAIN          #
+                #####################
+
+
+######################
+### Parse GFF input #
+my ($hash_omniscient, $hash_mRNAGeneLink) = BILS::Handler::GFF3handler->slurp_gff3_file_JD($gff);
+print ("GFF3 file parsed\n");
+
+
+foreach my $tag_l1 (keys %{$hash_omniscient->{'level1'}}){
+  foreach my $id_l1 (keys %{$hash_omniscient->{'level1'}{$tag_l1}}){
+        
+    my $feature_l1=$hash_omniscient->{'level1'}{$tag_l1}{$id_l1};
+        
+    manage_attributes($feature_l1, 'level1', \@ptagList,\%attListOk);
+
+    #################
+    # == LEVEL 2 == #
+    #################
+    foreach my $tag_l2 (keys %{$hash_omniscient->{'level2'}}){ # primary_tag_key_level2 = mrna or mirna or ncrna or trna etc...
+      
+      if ( exists ($hash_omniscient->{'level2'}{$tag_l2}{$id_l1} ) ){
+        foreach my $feature_l2 ( @{$hash_omniscient->{'level2'}{$tag_l2}{$id_l1}}) {
+          
+          manage_attributes($feature_l2,'level2',, \@ptagList,\%attListOk);
+          #################
+          # == LEVEL 3 == #
+          #################
+          my $level2_ID = lc($feature_l2->_tag_value('ID'));
+
+          foreach my $tag_l3 (keys %{$hash_omniscient->{'level3'}}){ # primary_tag_key_level3 = cds or exon or start_codon or utr etc...
+            if ( exists ($hash_omniscient->{'level3'}{$tag_l3}{$level2_ID} ) ){
+              foreach my $feature_l3 ( @{$hash_omniscient->{'level3'}{$tag_l3}{$level2_ID}}) {
+                manage_attributes($feature_l3, 'level3', \@ptagList,\%attListOk);
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+#print "We added $nbNameAdded Name attributes\n";
+
+
+#######################################################################################################################
+        ####################
+         #     methods    #
+          ################
+           ##############
+            ############
+             ##########
+              ########
+               ######
+                ####
+                 ##
+
+sub  manage_attributes{
+  my  ($feature, $level, $ptagList, $attListOk)=@_;
+
+  my $primary_tag=$feature->primary_tag;
+
+  # check primary tag (feature type) to handle
+  foreach my $ptag (@$ptagList){
+
+    if($ptag eq "all"){
+      remove_tag_from_list($feature,$attListOk);
+    }
+    elsif(lc($ptag) eq $level){
+      remove_tag_from_list($feature,$attListOk);
+    }
+    elsif(lc($ptag) eq lc($primary_tag) ){
+      remove_tag_from_list($feature,$attListOk);
+    }
+  }
+}
+
+sub remove_tag_from_list{
+  my  ($feature, $attListOk)=@_;
+
+  foreach my $att (sort keys %{$attListOk}){
+    if ($feature->has_tag($att)){
+      my @values = $feature->get_tag_values($att);
+      print $out @values."\n";
+    } 
+  }
+}
+
+
+__END__
+
+=head1 NAME
+
+gff3_extractAttributes.pl -
+The script take a gff3 file as input. -
+The script allows to remove choosen attributes to choosen features. 
+
+=head1 SYNOPSIS
+
+    ./gff3_extractAttributes.pl -gff file.gff  -att locus_tag,product,name -p level2,cds,exon [ -o outfile ]
+    ./gff3_extractAttributes.pl --help
+
+=head1 OPTIONS
+
+=over 8
+
+=item B<--gff> or B<-f>
+
+Input GFF3 file that will be read (and sorted)
+
+=item B<-p>,  B<-t> or  B<-l>
+
+primary tag option, case insensitive, list. Allow to specied the feature types that will be handled. 
+You can specified a specific feature by given its primary tag name (column 3) as: cds, Gene, MrNa
+You can specify directly all the feature of a particular level: 
+      level2=mRNA,ncRNA,tRNA,etc
+      level3=CDS,exon,UTR,etc
+By default all feature are taking in account. fill the option by the value "all" will have the same behaviour.
+
+=item B<-attributes>, B<--att>, B<-a>
+
+Attributes specified, will be extracted from the feature type specified by the option p (primary tag). List of attributes must be coma separated.
+/!\\ You must use "" if name contains spaces.
+
+=item B<-o> , B<--output> , B<--out> or B<--outfile>
+
+Output GFF file.  If no output file is specified, the output will be
+written to STDOUT.
+
+=item B<-h> or B<--help>
+
+Display this helpful text.
+
+=back
+
+=cut
