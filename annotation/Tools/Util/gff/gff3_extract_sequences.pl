248d57968b35d82ab5d625d94ba23781b9b2c2fa
Jacques Dainat <jacques.dainat@bils.se>
2016-07-04 13:01:21 +0200

    add new options and fixes

--- annotation/Tools/Util/gff/gff3_extract_sequences.pl
+++ annotation/Tools/Util/gff/gff3_extract_sequences.pl
@@ -1,11 +1,5 @@
 #!/usr/bin/perl
 
-###### Output type description ######
-# cds - translated sequence (starting with ATG and ending with a stop codon included)
-# cdna - transcribed sequence (devoid of introns, but containing untranslated exons)
-# protein - cds translated (includes a * as the stop codon)
-# gene - the entire gene sequence (including UTRs and introns)
-# upstream3000 - the 3000 upstream region of the gene (likely including the promoter)
 use strict;
 use Pod::Usage;
 use Getopt::Long;
@@ -24,6 +18,8 @@ my $opt_AA=undef;
 my $opt_help = 0;
 my $opt_extermityOnly=undef;
 my $opt_upstreamRegion=undef;
+my $opt_downRegion=undef;
+my $opt_cdna=undef;
 my $opt_type = 'cds';
 my $width = 60; # line length printed
 
@@ -36,12 +32,15 @@ my $header = qq{
 };
 
 # OPTION MANAGMENT
+my @copyARGV=@ARGV;
 if ( !GetOptions( 'g|gff=s' => \$opt_gfffile,
                   'f|fa|fasta=s' => \$opt_fastafile,
                   't=s' => \$opt_type,
                   'protein|p|aa' => \$opt_AA,
+                  'cdna' => \$opt_cdna,
                   'ext|e' => \$opt_extermityOnly,
-                  'up=i'      => \$opt_upstreamRegion,
+                  'up|5|five|upstream=i'      => \$opt_upstreamRegion,
+                  'do|3|three|down|downstream=i'      => \$opt_downRegion,
                   'o|output=s'      => \$opt_output,
                   'h|help!'         => \$opt_help ) )
 {
@@ -49,6 +48,8 @@ if ( !GetOptions( 'g|gff=s' => \$opt_gfffile,
                  -verbose => 1,
                  -exitval => 1 } );
 }
+# shortcut for cdna
+if($opt_cdna){$opt_type="exon";}
 
 # Print Help and exit
 if ($opt_help) {
@@ -57,7 +58,7 @@ if ($opt_help) {
                  -message => "$header \n" } );
 }
  
-if ( (! (defined($opt_gfffile)) ) || (! (defined($opt_fastafile)) ) ){
+if ( (! (defined($opt_gfffile)) ) and (! (defined($opt_fastafile)) ) ){
     pod2usage( {
            -message => "\nAt least 2 parametes are mandatory:\nInput reference gff file (-g);  Input reference fasta file (-f)\n\n".
            "Output is optional. Look at the help documentation to know more.\n",
@@ -77,15 +78,17 @@ else{
   $ostream = Bio::SeqIO->new(-fh => \*STDOUT, -format => 'Fasta');
 }
 
-##### MAIN ####
+
 if($opt_extermityOnly){
   print "option extremities activated => good for "
 }
-if($opt_upstreamRegion and ! $opt_extermityOnly){print "I don't use upstream region option without the \"extermity only\" option (ext) to avoid issue with multi-features !"; exit;}
+if( ($opt_upstreamRegion or $opt_downRegion)  and ! $opt_extermityOnly){print "I don't use upstream region option without the \"extermity only\" option (ext) to avoid issue with multi-features !"; exit;}
 
 print "We will extract the $opt_type sequences.\n";
 $opt_type=lc($opt_type);
 
+
+##### MAIN ####
 #### read gff file and save info in memory
 ######################
 ### Parse GFF input #
@@ -99,7 +102,6 @@ my $hash_l1_grouped = group_l1features_from_omniscient($hash_omniscient);
 
 #### read fasta
 my $nbFastaSeq=0;
-#my $inFasta  = Bio::SeqIO->new(-file => "$opt_fastafile" , '-format' => 'Fasta');
 my $db = Bio::DB::Fasta->new($opt_fastafile);
 print ("Genome fasta parsed\n");
 
@@ -116,8 +118,22 @@ foreach my $seqname (keys %{$hash_l1_grouped}) {
     }
 
     if($opt_type eq lc($feature_l1->primary_tag()) ){
+
+      #Handle Header
+      my $header=$id_l1;
+      if($name){
+        $header.="|Name=".$name;
+      }
+      $seqname =~ tr/|/_/;
+      $header.="|Seq_id=".$seqname."|type=".$opt_type;
+
       my @ListSeq=($feature_l1);
-      my $seqObj = extract_sequence(\@ListSeq, $db, $opt_extermityOnly, $opt_upstreamRegion);
+      my ($seqObj, $info) = extract_sequence(\@ListSeq, $db, $opt_extermityOnly, $opt_upstreamRegion, $opt_downRegion);
+      if($info){
+        $header.="|".$info;
+      }
+      $seqObj->id($header);
+
       print_seqObj($ostream, $seqObj, $opt_AA);
     }
 
@@ -128,13 +144,6 @@ foreach my $seqname (keys %{$hash_l1_grouped}) {
          
       if ( exists ($hash_omniscient->{'level2'}{$ptag_l2}{$id_l1} ) ){
         foreach my $feature_l2 ( @{$hash_omniscient->{'level2'}{$ptag_l2}{$id_l1}}) {
-
-
-          if($opt_type eq $ptag_l2){
-            my @ListSeq=($feature_l2);
-            my $seqObj = extract_sequence(\@ListSeq, $db, $opt_extermityOnly, $opt_upstreamRegion);
-            print_seqObj($ostream, $seqObj, $opt_AA);
-          }
 
           #For Header
           my $id_l2  = lc($feature_l2->_tag_value('ID'));
@@ -153,16 +162,28 @@ foreach my $seqname (keys %{$hash_l1_grouped}) {
           $seqname =~ tr/|/_/;
           $header.="|Seq_id=".$seqname."|type=".$opt_type;
 
+          if($opt_type eq $ptag_l2){
+            my @ListSeq=($feature_l2);
+            my ($seqObj, $info) = extract_sequence(\@ListSeq, $db, $opt_extermityOnly, $opt_upstreamRegion, $opt_downRegion);
+            if($info){
+              $header.="|".$info;
+            }
+            $seqObj->id($header);
+            
+            print_seqObj($ostream, $seqObj, $opt_AA);
+          }
 
           #################
           # == LEVEL 3 == #
           #################
           foreach my $ptag_l3 (keys %{$hash_omniscient->{'level3'}}){
             if ( exists ($hash_omniscient->{'level3'}{$ptag_l3}{$id_l2} ) ){
-                   
+              
               if( $opt_type eq $ptag_l3 ){
-                my $seqObj = extract_sequence(\@{$hash_omniscient->{'level3'}{$ptag_l3}{$id_l2}}, $db, $opt_extermityOnly, $opt_upstreamRegion);
-                     
+                my ($seqObj, $info) = extract_sequence(\@{$hash_omniscient->{'level3'}{$ptag_l3}{$id_l2}}, $db, $opt_extermityOnly, $opt_upstreamRegion, $opt_downRegion);
+                if($info){
+                  $header.="|".$info;
+                }
                 $seqObj->id($header);
                 #print 
                 print_seqObj($ostream,, $seqObj, $opt_AA);
@@ -174,8 +195,18 @@ foreach my $seqname (keys %{$hash_l1_grouped}) {
     }
   }
 }
-if($opt_upstreamRegion){
+
+#END
+print "usage: $0 @copyARGV\n";
+
+if($opt_upstreamRegion and $opt_downRegion){
+  print "$nbFastaSeq $opt_type converted in fasta with $opt_upstreamRegion upstream nucleotides and $opt_downRegion downstream nucleotides.\n";
+}
+elsif($opt_upstreamRegion){
   print "$nbFastaSeq $opt_type converted in fasta with $opt_upstreamRegion upstream nucleotides.\n";
+}
+elsif($opt_downRegion){
+  print "$nbFastaSeq $opt_type converted in fasta with $opt_downRegion downstream nucleotides.\n";
 }
 else{
   print "$nbFastaSeq $opt_type converted in fasta.\n";
@@ -198,18 +229,57 @@ print "Job done in $run_time seconds\n";
                  ##          
 
 sub extract_sequence{
-  my($feature_list, $db, $extermityOnly, $opt_upstreamRegion)=@_;
+  my($feature_list, $db, $extermityOnly, $opt_upstreamRegion, $opt_downRegion)=@_;
 
   my $sequence="";
+  my $info="";
   if($extermityOnly){
 
     my ($start, $end) = get_extremities($feature_list);
 
+    #5'
     if($opt_upstreamRegion){
-      $start=$start-$opt_upstreamRegion;
-      $end=$end+$opt_upstreamRegion;
-      if($start < 0){$start=0;}
-      if($end > $db->length($feature_list->[0]->seq_id) ){$end=length($feature_list->[0]->seq_id);}
+      #negative strand
+      if($feature_list->[0]->strand eq "-1" or $feature_list->[0]->strand eq "-"){
+        $end=$end+$opt_upstreamRegion;
+        
+        #get info
+        if($end > $db->length($feature_list->[0]->seq_id) ){
+          $info.="5'extra=".($db->length($feature_list->[0]->seq_id)-$end-$opt_upstreamRegion)."nt" ;
+        }
+        else{$info.="5'extra=".$opt_upstreamRegion."nt";}
+      }
+      else{
+        $start=$start-$opt_upstreamRegion;        
+
+        if($start < 0){
+          $info.="5'extra=".($start+$opt_upstreamRegion)."nt";
+        }
+        else{
+          $info.="5'extra=".$opt_upstreamRegion."nt";
+        }
+      }
+    }
+    #3'
+    if($opt_downRegion){
+      if($feature_list->[0]->strand eq "-1" or $feature_list->[0]->strand eq "-"){   
+        $start=$start-$opt_upstreamRegion;
+
+        #get info
+        if($start < 0){
+          $info.="3'extra=".($start+$opt_upstreamRegion)."nt";
+        }
+        else{$info.="3'extra=".$opt_upstreamRegion."nt";}
+      }
+      else{
+        $end=$end+$opt_upstreamRegion;
+
+        #get info
+        if($end > $db->length($feature_list->[0]->seq_id) ){
+          $info.="3'extra=".$db->length($feature_list->[0]->seq_id)-$end-$opt_upstreamRegion."nt" ;
+        }
+        else{$info.="3'extra=".$opt_upstreamRegion."nt";}
+      }
     }
 
     $sequence = $db->subseq($feature_list->[0]->seq_id, $start, $end);
@@ -228,7 +298,7 @@ sub extract_sequence{
     $seq=$seq->revcom;
   }
 
-  return $seq ;
+  return $seq,$info ;
 }
 
 sub get_extremities{
@@ -276,8 +346,16 @@ __END__
 =head1 NAME
 
 gff3_extract_cds_sequences.pl -
-This script ... 
+This script extract sequence in feasta format from gff file. You can extract the fasta of any kind of feature define by the 2th column in the gff file.
 The result is written to the specified output file, or to STDOUT.
+
+The Header are formated like that:
+>mRNA_ID|gene_ID|Name=NAME|Seq_id=Chromosome_ID|type=cds|5'extra=VALUE
+
+/!\ mRNA_ID not displayed when extracting gene. 
+Name is optional and will be written only if the Name attribute exists in th gff.
+type will be the feature type extracted. 
+5'extra or 3'extra is otpional, according to the use of the upstream and downstream option.
 
 =head1 SYNOPSIS
 
@@ -294,15 +372,34 @@ Input GFF3 file that will be read (and sorted)
 
 =item B<-f> or B<--fasta> 
 
-Input fasta file that will be masked
+Input fasta file.
 
-=item B<-sm> 
+=item B<-t> 
 
-SoftMask option =>Sequences masked will be in lowercase
+Define the feature you want to extract the sequnce from. By deafault it's 'cds'. Most common choice are: gene,mrna,exon,cds,trna,three_prime_utr,five_prime_utr.
 
-=item B<-hm> 
+=item B<-p>, B<--protein> or B<--aa>
 
-HardMask option => Sequences masked will be replaced by a character. By default the character used is 'n'. But you are allowed to speceify any character of your choice. To use 'z' instead of 'n' type: -hm z
+Will translate the extracted sequence in Amino acid.
+
+=item B<-e> or B<--ext>
+
+This option called "extremities" allows dealing with multifeature like cds or exon, to extract the full sequence from start extremity to the end extremity, i.e with introns.
+Use of that option with exon will give the same result as extract the mrna sequence.
+Use of that option on cds will give the cdna wihtout the untraslated sequences.
+
+=item B<-u>, B<--up>, B<-5>, B<--five> or B<-upstream>
+
+Integer. It will take that number of nucleotide in more at the 5' extremity. Option "e" must be activated to use this option (Why ? to avoid to extract intronic/overlaping sequence in case of feature spread over several locations (exon,cds,utrs)).
+
+
+=item B<-d>, B<--do>, B<-3>, B<--three>, B<-down> or B<-downstream>
+
+Integer. It will take that number of nucleotide in more at the 3' extremity. Option "e" must be activated to use this option (Why ? to avoid to extract intronic/overlaping sequence in case of feature spread over several locations (exon,cds,utrs)).
+
+=item B<--cdna>
+
+This extract the cdna sequence (i.e transcribed sequence (devoid of introns, but containing untranslated exons)). It correspond to extract the exons sequences.
 
 =item B<-o> or B<--output>
 
