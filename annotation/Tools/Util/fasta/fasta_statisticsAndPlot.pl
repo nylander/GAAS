070d271979483d150621b386ad1c9a1f4949712a
Jacques Dainat <jacques.dainat@bils.se>
2018-06-15 10:14:12 +0200

    update fasta_statisticsAndPlot.pl

--- annotation/Tools/Util/fasta/fasta_statisticsAndPlot.pl
+++ annotation/Tools/Util/fasta/fasta_statisticsAndPlot.pl
@@ -12,6 +12,7 @@ use POSIX qw(strftime);
 use File::Basename;
 use Pod::Usage;
 use Getopt::Long;
+use Try::Tiny;
 use Carp;
 use IO::File;
 
@@ -135,7 +136,6 @@ foreach my $key (keys %sequence){
   #Count lowercase outside Ns
   my $lowerCaseCount += ($noNs =~ tr/atgc/atgc/);
   $total_lowerCaseCount += $lowerCaseCount;
-};
 }
 
 #Calculate some statistics
@@ -185,101 +185,106 @@ print $outstream "$StingToPrint";
 #
 ######
 if($opt_dirRes){
-	
-	print $StingToPrint;
-	print "This result was saved in the $opt_dirRes directory.\nThe plots are in <pdf> format and available in the directory.\n";
+	try {
+		print $StingToPrint;
+		print "This result was saved in the $opt_dirRes directory.\nThe plots are in <pdf> format and available in the directory.\n";
 
-	# temporary file name
-	my $tempFile1="dump.tmp";
+		# temporary file name
+		my $tempFile1="dump.tmp";
 
-	# write the data in temporary file
-	open(FILE, ">$tempFile1") || die "Erreur E/S:$!\n";
-	foreach my $size ( @sequencelength ) {
-	  print FILE "$size\n";
+		# write the data in temporary file
+		open(FILE, ">$tempFile1") || die "Erreur E/S:$!\n";
+		foreach my $size ( @sequencelength ) {
+		  print FILE "$size\n";
+		}
+		close(FILE);
+
+
+
+		my $ouputName=basename($opt_infile);
+		my $ouputPlot=$opt_dirRes."/".$ouputName;
+
+
+
+		# Calcul percentage of contig right and left to N50
+		my $percentContigRightN50=($nbcontig*100)/($#sequencelength+1);
+		my $percentContigLeftN50=100-$percentContigRightN50;
+		$percentContigRightN50=sprintf ("%0.2f",$percentContigRightN50)."%";
+		$percentContigLeftN50=sprintf ("%0.2f",$percentContigLeftN50)."%";
+		# Name of different outputs
+		my $outputPlotLog=$ouputPlot."_PlotLog.pdf";
+		my $outputPlotDensity=$ouputPlot."_PlotDensity.pdf";
+		my $outputPlotHist=$ouputPlot."_PlotHisto.pdf";
+		# calcul right and left position to write percentContigRightN50 and percentContigLeftN50
+		my $biggestValue=shift @sequencelength;
+		my $positionright=(5*$biggestValue)/100+$entry;
+		my $positionleft=$entry-(5*$biggestValue)/100;
+		# Tab=as.matrix(read.table("$tempFile1", sep="\t", he=T)) 
+		# R object Declaration
+		my $R = Statistics::R->new() or die "Problem with R : $!\n";
+
+	## info ##
+	#myhist$breaks contient la valeur minimale de chaque intervalle
+	#myhist$mids contient la valeur au milieu de chaque intervalle
+	#myhist$counts contient le nombre de valeurs situées dans cet intervalle
+	#myhist$density contient la proportion de valeurs situées dans cet intervalle (autrement dit, tec.hist$counts / length (tec)).
+
+		# R command
+		 $R->send(
+		     qq`
+		  listValues=as.matrix(read.table("$tempFile1", sep="\t", he=F))
+		  myhist<-hist(listValues)
+		  legendToDisplay=paste("Number of value used : ",length(listValues))
+
+		  pdf("$outputPlotLog")
+		  plot(x = log(myhist\$mids), y = log(myhist\$counts), xlab="log(Contig size)", ylab="log(Frequency)", main="Size distribution of contigs")
+		  abline(v =log($entry), col=2)
+		  axisValues=par("usr")
+		  ymax=(axisValues[4]*90)/100
+		  ymax2=(axisValues[4]*85)/100
+		  shiftFivePercent=(5*(axisValues[2]-axisValues[1]))/100
+		  text(x = log($entry)+shiftFivePercent, y = ymax2, paste("$percentContigRightN50"), cex = 1, col = "red")
+		  text(x = log($entry)-shiftFivePercent, y = ymax2, paste("$percentContigLeftN50"), cex = 1, col = "red")
+		  text(x = log($entry), y = ymax, paste("N50"), cex = 1, col = "red")
+		  legend("topright", col=(1), lty=1, c(legendToDisplay))
+		  dev.off()
+
+		  pdf("$outputPlotDensity")
+		  plot(density(listValues), xlab="Contig size", main="Size distribution of contigs")
+		  abline(v =$entry, col=2)
+		  axisValues=par("usr")
+		  ymax=(axisValues[4]*90)/100
+		  ymax2=(axisValues[4]*85)/100
+		  text(x = $positionright, y = ymax2, paste("$percentContigRightN50"), cex = 1, col = "red")
+		  text(x = $positionleft, y = ymax2, paste("$percentContigLeftN50"), cex = 1, col = "red")
+		  text(x = $entry, y = ymax, paste("N50"), cex = 1, col = "red")  
+		  legend("topright", col=(1), lty=1, c(legendToDisplay))
+		  dev.off()
+		  
+		  pdf("$outputPlotHist")
+		  hist(listValues, xlab="Contig size",main="Size distribution of contigs")
+		  abline(v =$entry, col=2)
+		  axisValues=par("usr")
+		  ymax=(axisValues[4]*90)/100
+		  ymax2=(axisValues[4]*85)/100
+		  text(x = $positionright, y = ymax2, paste("$percentContigRightN50"), cex = 1, col = "red")
+		  text(x = $positionleft, y = ymax2, paste("$percentContigLeftN50"), cex = 1, col = "red")
+		  text(x = $entry, y = ymax, paste("N50"), cex = 1, col = "red")
+		  legend("topright", col=(1), lty=1, c(legendToDisplay))
+		  dev.off()`
+		     );
+
+		# Close the bridge
+		$R->stopR();
+
+		# Delete temporary file
+		unlink $tempFile1;
+		unlink "Rplots.pdf"; #created by " myhist<-hist(listValues)". I do not know how do differently...
 	}
-	close(FILE);
-
-
-
-	my $ouputName=basename($opt_infile);
-	my $ouputPlot=$opt_dirRes."/".$ouputName;
-
-
-
-	# Calcul percentage of contig right and left to N50
-	my $percentContigRightN50=($nbcontig*100)/($#sequencelength+1);
-	my $percentContigLeftN50=100-$percentContigRightN50;
-	$percentContigRightN50=sprintf ("%0.2f",$percentContigRightN50)."%";
-	$percentContigLeftN50=sprintf ("%0.2f",$percentContigLeftN50)."%";
-	# Name of different outputs
-	my $outputPlotLog=$ouputPlot."_PlotLog.pdf";
-	my $outputPlotDensity=$ouputPlot."_PlotDensity.pdf";
-	my $outputPlotHist=$ouputPlot."_PlotHisto.pdf";
-	# calcul right and left position to write percentContigRightN50 and percentContigLeftN50
-	my $biggestValue=shift @sequencelength;
-	my $positionright=(5*$biggestValue)/100+$entry;
-	my $positionleft=$entry-(5*$biggestValue)/100;
-	# Tab=as.matrix(read.table("$tempFile1", sep="\t", he=T)) 
-	# R object Declaration
-	my $R = Statistics::R->new() or die "Problem with R : $!\n";
-
-## info ##
-#myhist$breaks contient la valeur minimale de chaque intervalle
-#myhist$mids contient la valeur au milieu de chaque intervalle
-#myhist$counts contient le nombre de valeurs situées dans cet intervalle
-#myhist$density contient la proportion de valeurs situées dans cet intervalle (autrement dit, tec.hist$counts / length (tec)).
-
-	# R command
-	 $R->send(
-	     qq`
-	  listValues=as.matrix(read.table("$tempFile1", sep="\t", he=F))
-	  myhist<-hist(listValues)
-	  legendToDisplay=paste("Number of value used : ",length(listValues))
-
-	  pdf("$outputPlotLog")
-	  plot(x = log(myhist\$mids), y = log(myhist\$counts), xlab="log(Contig size)", ylab="log(Frequency)", main="Size distribution of contigs")
-	  abline(v =log($entry), col=2)
-	  axisValues=par("usr")
-	  ymax=(axisValues[4]*90)/100
-	  ymax2=(axisValues[4]*85)/100
-	  shiftFivePercent=(5*(axisValues[2]-axisValues[1]))/100
-	  text(x = log($entry)+shiftFivePercent, y = ymax2, paste("$percentContigRightN50"), cex = 1, col = "red")
-	  text(x = log($entry)-shiftFivePercent, y = ymax2, paste("$percentContigLeftN50"), cex = 1, col = "red")
-	  text(x = log($entry), y = ymax, paste("N50"), cex = 1, col = "red")
-	  legend("topright", col=(1), lty=1, c(legendToDisplay))
-	  dev.off()
-
-	  pdf("$outputPlotDensity")
-	  plot(density(listValues), xlab="Contig size", main="Size distribution of contigs")
-	  abline(v =$entry, col=2)
-	  axisValues=par("usr")
-	  ymax=(axisValues[4]*90)/100
-	  ymax2=(axisValues[4]*85)/100
-	  text(x = $positionright, y = ymax2, paste("$percentContigRightN50"), cex = 1, col = "red")
-	  text(x = $positionleft, y = ymax2, paste("$percentContigLeftN50"), cex = 1, col = "red")
-	  text(x = $entry, y = ymax, paste("N50"), cex = 1, col = "red")  
-	  legend("topright", col=(1), lty=1, c(legendToDisplay))
-	  dev.off()
-	  
-	  pdf("$outputPlotHist")
-	  hist(listValues, xlab="Contig size",main="Size distribution of contigs")
-	  abline(v =$entry, col=2)
-	  axisValues=par("usr")
-	  ymax=(axisValues[4]*90)/100
-	  ymax2=(axisValues[4]*85)/100
-	  text(x = $positionright, y = ymax2, paste("$percentContigRightN50"), cex = 1, col = "red")
-	  text(x = $positionleft, y = ymax2, paste("$percentContigLeftN50"), cex = 1, col = "red")
-	  text(x = $entry, y = ymax, paste("N50"), cex = 1, col = "red")
-	  legend("topright", col=(1), lty=1, c(legendToDisplay))
-	  dev.off()`
-	     );
-
-	# Close the bridge
-	$R->stopR();
-
-	# Delete temporary file
-	unlink $tempFile1;
-	unlink "Rplots.pdf"; #created by " myhist<-hist(listValues)". I do not know how do differently...
+	catch{
+		warn "caught error: $_";
+		unlink $tempFile1;
+	}
 }
 
 __END__
