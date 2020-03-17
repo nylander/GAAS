8d3bd0044909842806a00bcce2926eb664cf1252
Jacques Dainat <jacques.dainat@nbis.se>
2020-03-17 21:06:58 +0100

    quoting variable expansions and logically separating error handling from the main processing of the script

--- annotation/tools/abinitio/snap_train.sh
+++ annotation/tools/abinitio/snap_train.sh
@@ -2,12 +2,12 @@
 
 NAME=$1
 
-if [ -z "$NAME" ]
-then
-	echo "Must provide a name!"
-else
-	fathom -categorize 1000 genome.ann genome.dna
-	fathom -export 1000 -plus uni.ann uni.dna
-	forge export.ann export.dna
-	hmm-assembler.pl $NAME . > $NAME.hmm
+if [ -z "$NAME" ]; then
+        echo 'Must provide a name!' >&2
+        exit 1
 fi
+
+fathom -categorize 1000 genome.ann genome.dna
+fathom -export 1000 -plus uni.ann uni.dna
+forge export.ann export.dna
+hmm-assembler.pl "$NAME" . >"$NAME.hmm"
