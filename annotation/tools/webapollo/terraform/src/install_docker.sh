139ac4feda7ef47a1b7375867fe512020bc74eab
nanjiangshu <nanjiang.shu@gmail.com>
2020-11-02 18:46:03 +0100

    installing docker from snap

--- annotation/tools/webapollo/terraform/src/install_docker.sh
+++ annotation/tools/webapollo/terraform/src/install_docker.sh
@@ -1,10 +1,17 @@
 #!/bin/bash 
-curl -fsSL https://get.docker.com/ | sh
 
+# install docker from snap
+sudo snap install docker
+# Create and join the docker group.
+sudo addgroup --system docker
+sudo adduser $USER docker
+newgrp docker
+sudo snap disable docker
+sudo snap enable docker
+
+# install docker-compose
 if [ ! -f   /usr/local/bin/docker-compose ];then
     curl -L https://github.com/docker/compose/releases/download/1.26.2/docker-compose-`uname -s`-`uname -m` > /tmp/docker-compose; chmod +x /tmp/docker-compose; sudo cp /tmp/docker-compose /usr/local/bin/docker-compose
 fi
 
-sudo usermod -G docker -a ubuntu
-sudo systemctl restart docker
 exit 0
