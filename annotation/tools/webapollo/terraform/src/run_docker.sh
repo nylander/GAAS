4f7b12c309584f35b8ad4de34a7bf8e9f630c052
nanjiangshu <nanjiang.shu@gmail.com>
2020-11-09 16:21:35 +0100

    clone GAAS repo to software

--- annotation/tools/webapollo/terraform/src/run_docker.sh
+++ annotation/tools/webapollo/terraform/src/run_docker.sh
@@ -2,6 +2,6 @@
 pushd /media/storage
 mkdir -p software
 pushd software
-git pull https://github.com/NBISweden/GAAS
+git clone https://github.com/NBISweden/GAAS
 
 sudo docker run --name webapollo -d -e CATALINA_OPTS=-Xmx14G -e APOLLO_ADMIN_EMAIL=adminuser -e APOLLO_ADMIN_PASSWORD=gW/aGeajRShQ3g  -v /mnt/sequences/data/:/data -v /mnt/sequences/postgres-data/:/var/lib/postgresql -p 8888:8080 gmod/apollo:latest
