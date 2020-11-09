fea99fa20dbd161811c21d777519f4a7faa0b3a5
nanjiangshu <nanjiang.shu@gmail.com>
2020-11-09 14:21:11 +0100

    create network, mount volume

--- annotation/tools/webapollo/terraform/main.tf
+++ annotation/tools/webapollo/terraform/main.tf
@@ -4,6 +4,33 @@ resource "openstack_compute_keypair_v2" "my-cloud-key" {
 
 }
 
+resource "openstack_networking_network_v2" "net_webap" {
+  name           = var.internal_network_name
+  admin_state_up = "true"
+}
+
+resource "openstack_networking_subnet_v2" "subnet_webap" {
+  name       = var.internal_subnet_name
+  network_id = openstack_networking_network_v2.net_webap.id
+  cidr       = "10.0.0.0/24"
+  ip_version = 4
+  dns_nameservers = ["8.8.8.8"]
+  enable_dhcp = "true"
+}
+
+resource "openstack_networking_router_v2" "router_webap" {
+  name                = var.router_name
+  admin_state_up      = "true"
+  external_network_id = var.external_network_id
+}
+
+resource "openstack_networking_router_interface_v2" "router_interface" {
+  router_id = openstack_networking_router_v2.router_webap.id
+  subnet_id = openstack_networking_subnet_v2.subnet_webap.id
+}
+
+
+
 resource "openstack_compute_secgroup_v2" "secgroup_webap" {
   name        = var.secgroup_name
   description = "security group"
@@ -35,16 +62,36 @@ resource "openstack_compute_secgroup_v2" "secgroup_webap" {
   }
 }
 
-resource "openstack_networking_floatingip_v2" "myip" {
+resource "openstack_networking_port_v2" "port_webap" {
+  name               = var.port_name
+  network_id         = openstack_networking_network_v2.net_webap.id
+  admin_state_up     = "true"
+  security_group_ids = [openstack_compute_secgroup_v2.secgroup_webap.id]
+
+  fixed_ip {
+    subnet_id  = openstack_networking_subnet_v2.subnet_webap.id
+    ip_address = "10.0.0.10"
+  }
+}
+
+resource "openstack_networking_floatingip_v2" "ip_webap" {
   pool = var.floating_ip_pool
 }
 
-resource "openstack_compute_instance_v2" "nj_webap" {
+resource "openstack_blockstorage_volume_v2" "vol_webap" {
+  name = "vol_webap"
+  size = var.size_volume
+}
+
+
+
+
+resource "openstack_compute_instance_v2" "instance_webap" {
   name            = var.instance_name
   image_name      = var.image_name
   flavor_name     = var.flavor_name
   key_pair        = openstack_compute_keypair_v2.my-cloud-key.name
-  security_groups = ["default", var.secgroup_name]
+  security_groups = ["default", openstack_compute_secgroup_v2.secgroup_webap.name ]
 
   network {
     name = var.internal_network_name
@@ -52,22 +99,27 @@ resource "openstack_compute_instance_v2" "nj_webap" {
 
 }
 
+resource "openstack_compute_volume_attach_v2" "volume_attachement_1" {
+  instance_id = openstack_compute_instance_v2.instance_webap.id
+  volume_id   = openstack_blockstorage_volume_v2.vol_webap.id
+}
 
-resource "openstack_compute_floatingip_associate_v2" "myip" {
-  floating_ip = openstack_networking_floatingip_v2.myip.address
-  instance_id = openstack_compute_instance_v2.nj_webap.id
-  fixed_ip    = openstack_compute_instance_v2.nj_webap.network[0].fixed_ip_v4
+
+resource "openstack_compute_floatingip_associate_v2" "ip_webap" {
+  floating_ip = openstack_networking_floatingip_v2.ip_webap.address
+  instance_id = openstack_compute_instance_v2.instance_webap.id
+  fixed_ip    = openstack_compute_instance_v2.instance_webap.network[0].fixed_ip_v4
 }
 
 resource "null_resource" "provision" {
-  depends_on = [openstack_compute_floatingip_associate_v2.myip]
+  depends_on = [openstack_compute_floatingip_associate_v2.ip_webap]
   connection {
     type = "ssh"
     user = var.ssh_user
     private_key = file(var.private_ssh_key)
-    agent  = true
+    agent  = "true"
     timeout  = "5m"
-    host = openstack_networking_floatingip_v2.myip.address
+    host = openstack_networking_floatingip_v2.ip_webap.address
   }
 
   provisioner "remote-exec" {
--- annotation/tools/webapollo/terraform/variables.tf
+++ annotation/tools/webapollo/terraform/variables.tf
@@ -4,7 +4,19 @@ variable floating_ip_pool {
 }
 variable internal_network_name {
     description = "Name of the internal network"
-    default = "SNIC 2020/20-30 Internal IPv4 Network"
+    default = "net_webap"
+}
+variable internal_subnet_name {
+    description = "Name of the internal subnet"
+    default = "subnet_webap"
+}
+variable port_name {
+    description = "Name of the port"
+    default = "port_webap"
+}
+variable router_name {
+    description = "Name of the router_name"
+    default = "router_webap"
 }
 variable ssh_user {
     description = "SSH user name (use the default user for the OS image)"
@@ -17,6 +29,9 @@ variable private_ssh_key {
 variable public_ssh_key {
     description = "Local path to the public SSH key"
     default =  "./private/webap.key.pub"
+}
+variable external_network_id {
+    description = "ID for the external network"
 }
 variable image_name {
     description = "Name of the image for the instance"
@@ -37,4 +52,8 @@ variable keypair_name {
 variable secgroup_name {
     description = "Name of seurity group"
     default = "secgroup-webap"
+}
+variable size_volume {
+    description = "size of the volume to be attached to the instance"
+    default = 100
 }
