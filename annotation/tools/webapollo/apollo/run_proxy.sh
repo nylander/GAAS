b1b1970f36595c52f4d68ee376444ae86020c1e8
Pontus Freyhult <pontus_github@soua.net>
2021-03-16 13:12:48 +0100

    Restart automatically (start upon boot)

--- annotation/tools/webapollo/apollo/run_proxy.sh
+++ annotation/tools/webapollo/apollo/run_proxy.sh
@@ -3,4 +3,4 @@
 here=$(realpath "$(dirname "$0")")
 mkdir -p ~/config/serve
 rsync -L -au "$here/web/" ~/config/serve/
-docker run --rm -p 80:8080 -p 443:8443 -v ~/config:/config:ro -v "$here/nginx.conf":/etc/nginx/nginx.conf:ro --name nginx -d nginx
+docker run --restart unless-stopped -p 80:8080 -p 443:8443 -v ~/config:/config:ro -v "$here/nginx.conf":/etc/nginx/nginx.conf:ro --name nginx -d nginx
