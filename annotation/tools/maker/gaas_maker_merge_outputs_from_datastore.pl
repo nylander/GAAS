6f62687b34048b7392436fef580a94bd89c82ca7
nylander <Johan.Nylander@nrm.se>
2021-04-23 15:49:35 +0200

    reformat

--- annotation/tools/maker/gaas_maker_merge_outputs_from_datastore.pl
+++ annotation/tools/maker/gaas_maker_merge_outputs_from_datastore.pl
@@ -48,7 +48,7 @@ if ($help) {
 my @inDir;
 my $dir = getcwd;
 
-if(! $in){
+if (! $in) {
     # Find the datastore index
     my $maker_dir = undef;
 
@@ -58,25 +58,27 @@ if(! $in){
 
     my (@matchedDir) = grep $_ =~ /^.*\.maker\.output$/ , @dirList ;
 
-    foreach my $makerDir (@matchedDir){
+    foreach my $makerDir (@matchedDir) {
         push(@inDir, $makerDir);
     }
 }
-else{
+else {
     if (! -d "$in") {
         die "The outdirectory $in doesn't exist.\n";
     }
-    else{
+    else {
         push(@inDir, $in);
     }
 }
 
 # MESSAGES
-my $nbDir=$#inDir+1;
-if ($nbDir == 0){die "There seems to be no maker output directory here, exiting...\n";}
+my $nbDir = $#inDir+1;
+if ($nbDir == 0) {
+    die "There seems to be no maker output directory here, exiting...\n";
+}
 print "We found $nbDir maker output directorie(s):\n";
-foreach my $makerDir (@inDir){
-        print "\t+$makerDir\n";
+foreach my $makerDir (@inDir) {
+    print "\t+$makerDir\n";
 }
 
 #CONSTANT
@@ -95,58 +97,61 @@ foreach my $makerDir (@inDir){
     my %file_hds;
     my $genomeName = $makerDir;
     $genomeName =~ s/\.maker\.output.*//;
-    my $maker_dir_path = $dir . "/" . $makerDir."/";
-    my $datastore = $maker_dir_path.$genomeName."_datastore" ;
+    my $maker_dir_path = $dir . "/" . $makerDir . "/";
+    my $datastore = $maker_dir_path . $genomeName . "_datastore" ;
 
 # --------------- check presence datastore ----------------------
-    if (-d $datastore ) {
-            print "Datastore folder found in $makerDir, merging annotations now...\n";
-    } else {
-            die "Could not find datastore index ($datastore), exiting...\n";
+    if (-d $datastore) {
+        print "Datastore folder found in $makerDir, merging annotations now...\n";
+    }
+    else {
+        die "Could not find datastore index ($datastore), exiting...\n";
     }
 # --------------- check output folder ----------------------
 
     my $outfolder = undef;
-    if ($output){
-        if ($nbDir == 1){
+    if ($output) {
+        if ($nbDir == 1) {
             $outfolder = $output;
         }
-        else{
-            $outfolder = $output."_$genomeName";
+        else {
+            $outfolder = $output . "_$genomeName";
         }
     }
-    else{ $outfolder = "maker_output_processed_$genomeName";}
+    else {
+        $outfolder = "maker_output_processed_$genomeName";
+    }
     if (-d "$outfolder") {
         print "The output directory <$outfolder> already exists, let's see if something is missing inside.\n";
     }
-    else{
+    else {
         print "Creating the $outfolder folder\n";
         mkdir $outfolder;
     }
 
 # --------------- GATHERING gff and fasta ----------------------
-    if ( ( grep -f, glob "$outfolder/*.fasta") or ( grep -f, glob "$outfolder/*.gff") ){
+    if ((grep -f, glob "$outfolder/*.fasta") or (grep -f, glob "$outfolder/*.gff")) {
         print "Output fasta/gff file already exists. We skip the gathering step.\n";
     }
-    else{
+    else {
         print "Now collecting gff and fasta files...\n";
         collect_recursive(\%file_hds, $datastore, $outfolder, $genomeName);
 
-        #Close all file_handler opened that are not gff (gff files created by awk)
-        foreach my $key (keys %file_hds){
+        # Close all file_handler opened that are not gff (gff files created by awk)
+        foreach my $key (keys %file_hds) {
             close $file_hds{$key};
         }
-        #add ##gff-version 3 header to all gff files
+        # Add ##gff-version 3 header to all gff files
         opendir(DIR, $outfolder);
-        my @gff_files = grep(/\.gff$/,readdir(DIR));
+        my @gff_files = grep(/\.gff$/, readdir(DIR));
         closedir(DIR);
 
         foreach my $gff_file (@gff_files) {
-            if($^O =~ "linux"){
-                   system "sed -i '1s/^/##gff-version 3\\\n/' $outfolder/$gff_file";
+            if ($^O =~ "linux") {
+                system "sed -i '1s/^/##gff-version 3\\\n/' $outfolder/$gff_file";
             }
-            else{
-                   system "sed -i '' '1s/^/##gff-version 3\\\n/' $outfolder/$gff_file"; # Mac syntax
+            else {
+                system "sed -i '' '1s/^/##gff-version 3\\\n/' $outfolder/$gff_file"; # Mac syntax
             }
         }
     }
@@ -156,14 +161,14 @@ foreach my $makerDir (@inDir){
     if (-f "$outfolder/maker_opts.ctl") {
         print "A copy of the Maker files already exists in $outfolder/maker_opts.ctl.  We skip it.\n";
     }
-    else{
-        if(! $in){
+    else {
+        if (! $in) {
             copy("maker_opts.ctl","$outfolder/maker_opts.ctl") or print "Copy failed: $! $outfolder/maker_opts.ctl\n";
             copy("maker_exe.ctl","$outfolder/maker_exe.ctl") or print "Copy failed: $! $outfolder/maker_exe.ctl\n";
             copy("maker_evm.ctl","$outfolder/maker_evm.ctl") or print "Copy failed: $! $outfolder/maker_evm.ctl\n";
             copy("maker_bopts.ctl","$outfolder/maker_bopts.ctl") or print "Copy failed: $! $outfolder/maker_bopts.ctl\n";
         }
-        else{
+        else {
             my ($name,$path,$suffix) = fileparse($in);
             copy("$path/maker_opts.ctl","$outfolder/maker_opts.ctl") or print  "Copy failed: $! $outfolder/maker_opts.ctl\n";
             copy("$path/maker_exe.ctl","$outfolder/maker_exe.ctl") or print "Copy failed: $! $outfolder/maker_exe.ctl\n";
@@ -182,7 +187,7 @@ foreach my $makerDir (@inDir){
     if (-f $annotation) {
         system "chmod 444 $annotation";
     }
-    else{
+    else {
         print "ERROR: Do not find the $annotation file !\n";
     }
 
@@ -192,11 +197,11 @@ foreach my $makerDir (@inDir){
     if (-f $annotation_stat) {
         print "$annotation_stat file already exsits...\n";
     }
-    else{
+    else {
         print "Now performing the statistics of the annotation file $annotation...\n";
         my $full_path = can_run('agat_sp_statistics.pl') or print "Cannot launch statistics. agat_sp_statistics.pl script not available\n";
         if ($full_path) {
-                system "agat_sp_statistics.pl --gff $annotation -o $annotation_stat > $outfolder/maker_annotation_parsing.log";
+            system "agat_sp_statistics.pl --gff $annotation -o $annotation_stat > $outfolder/maker_annotation_parsing.log";
         }
     }
     print "All done!\n";
@@ -217,74 +222,76 @@ foreach my $makerDir (@inDir){
 sub collect_recursive {
     my ($file_hds, $full_path, $out, $genomeName) = @_;
 
-    my ($name,$path,$suffix) = fileparse($full_path,qr/\.[^.]*/);
+    my ($name, $path, $suffix) = fileparse($full_path, qr/\.[^.]*/);
 
-    if( ! -d $full_path ){
+    if (! -d $full_path) {
 
         ###################
         # deal with fasta #
-        if($suffix eq ".fasta"){
+        if ($suffix eq ".fasta") {
             my $key = undef;
             my $type = undef;
-            if($name =~ /([^\.]+)\.transcripts/){
+            if ($name =~ /([^\.]+)\.transcripts/) {
                 $key = $1;
                 $type = "transcripts";
             }
-            if($name =~ /([^\.]+)\.proteins/){
+            if ($name =~ /([^\.]+)\.proteins/) {
                 $key = $1;
                 $type = "proteins";
             }
-            if($name =~ /([^\.]+)\.noncoding/){
+            if ($name =~ /([^\.]+)\.noncoding/) {
                 $key = $1;
                 $type = "noncoding";
             }
-            if($key){
-                my $prot_out_file_name=undef;
-                if ($key eq 'maker'){ # protein or transcript correspinding to the maker annotation
+            if ($key) {
+                my $prot_out_file_name = undef;
+                if ($key eq 'maker') { # protein or transcript correspinding to the maker annotation
                     $prot_out_file_name = "$maker_annotation_prefix.$type.fasta";
                 }
-                else{
+                else {
                     my $source = "maker.$key";
                     $prot_out_file_name = "$genomeName.all.$source.$type.fasta";
                 }
 
-                my $protein_out_fh=undef;
-                if( _exists_keys ($file_hds,($prot_out_file_name)) ){
+                my $protein_out_fh = undef;
+                if (_exists_keys ($file_hds,($prot_out_file_name))) {
                     $protein_out_fh = $file_hds->{$prot_out_file_name};
                 }
-                else{
+                else {
                     open($protein_out_fh, '>', "$out/$prot_out_file_name") or die "Could not open file '$out/$prot_out_file_name' $!";
-                    $file_hds->{$prot_out_file_name}=$protein_out_fh;
+                    $file_hds->{$prot_out_file_name} = $protein_out_fh;
                 }
 
-                #print
+                # Print
                 open(my $fh, '<:encoding(UTF-8)', $full_path) or die "Could not open file '$full_path' $!";
-                    while (<$fh>) {
-                        print $protein_out_fh $_;
-                    }
+                while (<$fh>) {
+                    print $protein_out_fh $_;
+                }
                 close $fh;
             }
         }
 
-        ################
-         #deal with gff #
-        if($suffix eq ".gff"){
+        #################
+        # Deal with gff #
+        if ($suffix eq ".gff") {
             system "awk -F '    ' 'NF==9 {print \$0 >> \"$out/$maker_mix_prefix.gff\"}' $full_path";
-                system "awk '{if(\$2 ~ /[a-zA-Z]+/) if(\$2==\"maker\") { print \$0 >> \"$out/$maker_annotation_prefix.gff\" } else { OFS=\"\\t\"; gsub(/:/, \"_\" ,\$2); print \$0 >> \"$out/\"\$2\".gff\" } }' $full_path";
+            system "awk '{if(\$2 ~ /[a-zA-Z]+/) if(\$2==\"maker\") { print \$0 >> \"$out/$maker_annotation_prefix.gff\" } else { OFS=\"\\t\"; gsub(/:/, \"_\" ,\$2); print \$0 >> \"$out/\"\$2\".gff\" } }' $full_path";
         }
 
         return;
     }
-    if($name =~ /^theVoid/){ # In the void there is sub results already stored in the up folder. No need to go such deep otherwise we will have duplicates.
+
+    if ($name =~ /^theVoid/) { # In the void there is sub results already stored in the up folder. No need to go such deep otherwise we will have duplicates.
         return;
     }
+
     opendir my $dh, $full_path or die;
     while (my $sub = readdir $dh) {
         next if $sub eq '.' or $sub eq '..';
-
         collect_recursive($file_hds, "$full_path/$sub", $out, $genomeName);
     }
     close $dh;
+
     return;
 }
 
@@ -297,6 +304,7 @@ sub _exists_keys {
         }
         return 1;
     }
+
     return '';
 }
 
